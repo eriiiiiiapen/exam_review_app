f8dd5f89cd8cbdb739d0d0bc89a40cabc55e3ab6
eriiiiiiapen <91891293+eriiiiiiapen@users.noreply.github.com>
2026-02-03 23:48:17 +0900

    科目一覧表示のテスト追加

--- spec/requests/exams_spec.rb
+++ spec/requests/exams_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Exams", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/exams/index"
+  let!(:exam) { create(:exam, name: "社労士") }
+  let!(:subject_labor) { create(:subject, exam: exam, name: "労働基準法") }
+  let!(:subject_health) { create(:subject, exam: exam, name: "健康保険法") }
+
+  describe "GET /exams/:id" do
+    it "正常にレスポンスを返し、科目一覧が表示されていること" do
+      get exam_path(exam)
+
       expect(response).to have_http_status(:success)
+      expect(response.body).to include("社労士")
+      expect(response.body).to include("労働基準法")
+      expect(response.body).to include("健康保険法")
     end
   end
-
-  describe "GET /show" do
-    it "returns http success" do
-      get "/exams/show"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
