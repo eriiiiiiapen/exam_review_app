82c4874bcc1c7c4024da8f20d45d95bc9bde4b9d
eriiiiiiapen <91891293+eriiiiiiapen@users.noreply.github.com>
2026-02-10 21:25:03 +0900

    論点のCSVインポート機能追加

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Topic < ApplicationRecord
   belongs_to :subject
   has_many :topic_tags, dependent: :destroy
@@ -17,4 +19,30 @@ class Topic < ApplicationRecord
   end
 
   validates :name, presence: true
+
+  def self.import_csv!(file)
+    subjects_cache = Subject.pluck(:name, :id).to_h
+
+    CSV.foreach(file.path, headers: true) do |row|
+      ActiveRecord::Base.transaction do
+        subject_id = subjects_cache[row['科目']]
+        unless subject_id
+          exam = Exam.first
+          new_subject = Subject.find_or_create_by!(name: row['科目'], exam: exam)
+          subject_id = new_subject.id
+          subjects_cache[row['科目']] = subject_id
+        end
+
+        topic = Topic.find_or_initialize_by(name: row['論点名'], subject_id: subject_id)
+        topic.description = row['説明']
+        topic.save!
+
+        if row['タグ'].present?
+          tag_names = row['タグ'].split(':').map(&:strip)
+          tags = tag_names.map { |name| Tag.find_or_create_by!(name: name) }
+          topic.tags = tags
+        end
+      end
+    end
+  end
 end
